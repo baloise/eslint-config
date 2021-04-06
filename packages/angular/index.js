88bc6c11ffa8dde8029eee7a745dfe1738b9c29b
hirsch <gery.hirschfeld@w3tec.ch>
2021-04-06 15:37:05 +0200

    feat(base): update

--- packages/angular/index.js
+++ packages/angular/index.js
@@ -1,5 +1,14 @@
-'use strict';
-
 module.exports = {
-
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["@baloise/eslint-config-base"],
+  rules: {
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "no-restricted-imports": ["error", { patterns: ["rxjs/internal/*"] }],
+  },
 };
